16ab5f94f98e3e0f6086ed3b01c1069d47e1ed7b
Romain Calascibetta <romain.calascibetta@gmail.com>
2015-08-10 00:19:41 +0200

    Use new inflater and instantiate Deflate module

--- bin/main.ml
+++ bin/main.ml
@@ -1,48 +1,50 @@
 open Decompress
 
 module Input =
-  struct
-    type t = Pervasives.in_channel
+    struct
+      type t = Pervasives.in_channel
+      type buffer = Bytes.t
 
-    let std = Pervasives.stdin
+      let std = Pervasives.stdin
 
-    let read_byte = Pervasives.input_byte
-    let input ch n =
-      if n <= 0 then Bytes.empty
-      else
-        let bytes = Bytes.create n in
-        let len = ref n in
-        let pos = ref 0 in
+      let read_byte = Pervasives.input_byte
+      let input ch n =
+        if n <= 0 then Bytes.empty
+        else
+          let bytes = Bytes.create n in
+          let len = ref n in
+          let pos = ref 0 in
 
-        try
-          while !len > 0 do
-            let rest = Pervasives.input ch bytes !pos !len in
-            if rest = 0 then raise End_of_file;
-            pos := !pos + rest;
-            len := !len - rest;
-          done;
+          try
+            while !len > 0 do
+              let rest = Pervasives.input ch bytes !pos !len in
+              if rest = 0 then raise End_of_file;
+              pos := !pos + rest;
+              len := !len - rest;
+            done;
 
-          bytes
-        with End_of_file as exn ->
-          if !pos = 0 then raise exn
-          else Bytes.sub bytes 0 !pos
+            bytes
+          with End_of_file as exn ->
+            if !pos = 0 then raise exn
+            else Bytes.sub bytes 0 !pos
+    end
 
-  end
-
-module Output =
-  struct
-    type t = Pervasives.out_channel
-
-    let std = Pervasives.stdout
-
-    let output_char = Pervasives.output_char
-    let output_bytes = Pervasives.output_bytes
-  end
-
-module Inflate = Inflate.Make(Input)(Output)
+module Inflate = Inflate.Make(Input)(struct include Bytes let of_bytes x = x end)
+module Deflate =
+  Deflate.Make(Input)(
+    Bitstream.StreamNativeInt(
+      Bitstream.Target(
+        Bitstream.NativeInt(
+          Bitstream.BufferNativeInt))))
 
 let () =
-  let inflater = Inflate.init Input.std Output.std in
-  try Inflate.eval inflater
+  let buffer = Bytes.create 1 in
+  let inflater = Inflate.init Input.std buffer in
+  try
+    while not (Inflate.finish inflater)
+    do Inflate.eval inflater;
+       Printf.printf "%s%!" buffer;
+       Inflate.clear inflater 1
+    done
   with exn ->
     Printexc.to_string exn |> Printf.fprintf stderr "%s\n%!"
