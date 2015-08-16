517d5ea688ce1fda7e1325337cc0d748ea585e83
Romain Calascibetta <romain.calascibetta@gmail.com>
2015-08-16 18:17:08 +0200

    Add option -d to use Inflate instead Deflate

--- bin/main.ml
+++ bin/main.ml
@@ -29,22 +29,56 @@ module Input =
             else Bytes.sub bytes 0 !pos
     end
 
+module Output =
+  Bitstream.StreamNativeInt(
+    Bitstream.Target(
+      Bitstream.NativeInt(
+        Bitstream.BufferNativeInt)))
+
 module Inflate = Inflate.Make(Input)(struct include Bytes let of_bytes x = x end)
-module Deflate =
-  Deflate.Make(Input)(
-    Bitstream.StreamNativeInt(
-      Bitstream.Target(
-        Bitstream.NativeInt(
-          Bitstream.BufferNativeInt))))
+module Deflate = Deflate.Make(Input)(Output)
+
+type t =
+  | Inflate
+  | Deflate
+
+let () = Printexc.record_backtrace true
 
 let () =
+  let output = Output.create 16 in
   let buffer = Bytes.create 1 in
   let inflater = Inflate.init Input.std buffer in
-  try
-    while not (Inflate.finish inflater)
-    do Inflate.eval inflater;
-       Printf.printf "%s%!" buffer;
-       Inflate.clear inflater 1
-    done
+  let deflater = Deflate.init Input.std output in
+
+  let mode =
+    if Array.length Sys.argv = 2
+       && Sys.argv.(1) = "-d"
+    then Inflate
+    else Deflate
+  in
+
+  (* try *)
+    match mode with
+    | Inflate ->
+       while not (Inflate.finish inflater)
+      do Inflate.eval inflater;
+         Printf.printf "%s%!" buffer;
+         Inflate.clear inflater 1
+      done
+    | Deflate ->
+      while not (Deflate.finish deflater)
+      do Deflate.eval deflater
+      done;
+
+      Output.flush output;
+      Printf.printf "%s%!" (Output.contents output)
+  (*
   with exn ->
-    Printexc.to_string exn |> Printf.fprintf stderr "%s\n%!"
+    Printexc.to_string exn |> Printf.fprintf stderr "!> %s\n%!";
+
+    match mode with
+    | Deflate ->
+      List.iter (Printf.fprintf stderr "|> %s\n%!") (Deflate.trace deflater)
+    | Inflate ->
+      List.iter (Printf.fprintf stderr "|> %s\n%!") (Inflate.trace inflater)
+  *)
