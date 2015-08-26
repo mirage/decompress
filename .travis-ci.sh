0f4d0033d1d80f4b52796eea58252641e4b8f0ae
Romain Calascibetta <romain.calascibetta@gmail.com>
2015-08-26 19:24:44 +0200

    Update travis to upload documentation

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -10,3 +10,31 @@ eval `opam config env`
 
 ./configure
 make
+
+if [ "$TRAVIS_REPO_SLUG" == "oklm-wsh/Decompress" ] \
+     && [ "$TRAVIS_PULL_REQUEST" == "false" ] \
+     && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing ocamldoc...\n"
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone https://${GH_TOKEN}@github.com/oklm-wsh/Decompress .documentation
+
+  cd .documentation
+  git fetch
+  git checkout gh-pages
+  git merge master --commit -m "Merge master into gh-pages"
+
+  ./configure
+  make doc
+
+  git add -f doc/
+
+  if [ -n "$(git status --untracked-files=no --porcelain)" ]; then
+    git commit -m "Update documentation $TRAVIS_BUILD_NUMBER"
+    git push -fq origin gh-pages
+  fi
+
+  echo -e "Published ocamldoc to gh-pages.\n"
+fi
