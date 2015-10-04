ef72fff988dee6371057f4f8e5528ac6c54294dd
Romain Calascibetta <romain.calascibetta@gmail.com>
2015-10-04 15:39:26 +0200

    New main

--- bin/main.ml
+++ bin/main.ml
@@ -8,49 +8,97 @@ module Bytes =
     let of_bytes x = x
   end
 
-module Inflate = Decompress.Inflate.Make(Bytes)
-module Deflate = Decompress.Deflate.Make(Bytes)
+exception Inflate_error
+exception Deflate_error
 
-let pipe () =
-  let buffer = Buffer.create 0xFFFF in
-  let output = Bytes.create 0xFFFF in
+module Inflate =
+  struct
+    include Decompress.Inflate.Make(Bytes)
 
-  let inflate () =
+    let decompress ?(window_bits = 15) refill flush' =
+      let t = Bytes.create 0xFF in
+      let inflater = make (`Manual refill) t in
+      let rec aux () = match eval inflater with
+        | `Ok -> flush' t (contents inflater); flush inflater
+        | `Flush -> flush' t (contents inflater); flush inflater; aux ()
+        | `Error -> raise Inflate_error
+      in aux ()
 
-    let rec aux inflater = match Inflate.eval inflater with
-      | `Flush ->
-        Printf.printf "%s%!"
-          (Bytes.sub output 0 (Inflate.contents inflater)
-           |> Bytes.to_string);
-        Inflate.flush inflater;
-        aux inflater
-      | `Ok ->
-        if Inflate.contents inflater <> 0
-        then Printf.printf "%s%!"
-          (Bytes.sub output 0 (Inflate.contents inflater)
-           |> Bytes.to_string)
-        else ()
-      | `Error -> ()
-    in aux (Inflate.make (`String (0, Buffer.contents buffer)) output)
+    let decompress_string ?(window_bits = 15) str flush' =
+      let t = Bytes.create 0xFF in
+      let inflater = make (`String (0, str)) t in
+      let rec aux () = match eval inflater with
+        | `Ok -> flush' t (contents inflater); flush inflater
+        | `Flush -> flush' t (contents inflater); flush inflater; aux ()
+        | `Error -> raise Inflate_error
+      in aux ()
+  end
+
+module Deflate =
+  struct
+    include Decompress.Deflate.Make(Bytes)
+
+    let compress ?(window_bits = 15) refill flush' =
+      let t = Bytes.create 0xFF in
+      let deflater = make ~window_bits (`Manual refill) t in
+      let rec aux () = match eval deflater with
+        | `Ok -> flush' t (contents deflater); flush deflater
+        | `Flush -> flush' t (contents deflater); flush deflater; aux ()
+        | `Error -> raise Deflate_error
+      in aux ()
+
+    let compress_string ?(window_bits = 15) str flush' =
+      let t = Bytes.create 0xFF in
+      let deflater = make (`String (0, str)) t in
+      let rec aux () = match eval deflater with
+        | `Ok -> flush' t (contents deflater); flush deflater
+        | `Flush -> flush' t (contents deflater); flush deflater; aux ()
+        | `Error -> raise Deflate_error
+      in aux ()
+  end
+
+let () = Printexc.record_backtrace true
+
+let compress input =
+  let compressed = Buffer.create 16 in
+  let flush output buf len =
+    Buffer.add_subbytes output buf 0 len in
+
+  Deflate.compress_string input (flush compressed);
+
+  Buffer.contents compressed
+
+let decompress input =
+  let uncompressed = Buffer.create 16 in
+  let flush buf size =
+    Buffer.add_subbytes uncompressed buf 0 size in
+
+  Inflate.decompress_string input flush;
+
+  Buffer.contents uncompressed
+
+let read_line ic =
+  try Some (input_line ic)
+  with End_of_file -> None
+
+let read_lines ic =
+  let rec loop acc =
+    match read_line ic with
+    (* Unix only *)
+    | Some line -> loop ((line ^ "\n") :: acc)
+    | None -> List.rev acc
   in
+  loop []
 
-  let rec aux deflater = match Deflate.eval deflater with
-    | `Flush ->
-      Buffer.add_bytes
-        buffer
-        (Bytes.sub output 0 (Deflate.contents deflater));
-      Deflate.flush deflater;
-      aux deflater
-    | `Ok ->
-      begin
-        if Deflate.contents deflater <> 0
-        then begin
-          Buffer.add_bytes buffer
-            (Bytes.sub output 0 (Deflate.contents deflater));
-        end;
-        inflate ()
-      end
-    | `Error -> ()
-  in aux (Deflate.make (`Channel stdin) output)
+let load_input () =
+  read_lines stdin |> List.fold_left ( ^ ) ""
 
-let () = pipe ()
+let write_output str =
+  let _ = Unix.write_substring Unix.stdout str 0 (String.length str) in ()
+
+let () =
+  if Sys.argv |> Array.length >= 1
+  then if Sys.argv |> Array.length >= 2 && Sys.argv.(1) = "-d"
+    then decompress (load_input ()) |> write_output
+    else compress (load_input ()) |> write_output
+  else ()
