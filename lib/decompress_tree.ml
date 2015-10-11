ce97141b59224f564137cc430204c82bcd04f89c
Romain Calascibetta <romain.calascibetta@gmail.com>
2015-10-11 17:10:44 +0200

    Fix a *fu***** bug in reverse package merge algorithm.
    This algorithm fails when all frequencies equal 0. In this case
    (generally with distance frequencies in random string), the program
    raise [index of bound]. But, in Zlib, we add 2 dummy frequencies to
    create a minimal huffman tree - so this patch do this.

--- lib/decompress_tree.ml
+++ lib/decompress_tree.ml
@@ -100,15 +100,25 @@ let get_lengths freqs limit =
 
   begin
     let heap = Heap.make (2 * 286) in
+    let max_code = ref (-1) in
 
     Array.iteri
-      (fun i freq -> if freq > 0 then Heap.push i freq heap)
+      (fun i freq -> if freq > 0 then (max_code := i; Heap.push i freq heap))
       freqs;
 
-    let nodes = Array.make (Heap.length heap / 2) (0, 0) in
-    let values = Array.make (Heap.length heap / 2) 0 in
-
     try
+      (* The pkzip format requires that at least one distance code exists,
+         and that at least one bit should be sent even if there is only one
+         possible code. So to avoid special checks later on we force at least
+         two codes of non zero frequency. *)
+      while Heap.length heap / 2 < 2 do
+        Heap.push (if !max_code < 2 then !max_code + 1 else 0) 1 heap;
+        if !max_code < 2 then incr max_code;
+      done;
+
+      let nodes = Array.make (Heap.length heap / 2) (0, 0) in
+      let values = Array.make (Heap.length heap / 2) 0 in
+
       if Array.length nodes = 1
       then begin
         let index, value = Heap.pop heap in
