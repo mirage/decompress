7114e2f101f1f161f87106afc8b9a1abf80fce61
Romain Calascibetta <romain.calascibetta@gmail.com>
2016-04-06 06:33:35 -0700

    MOAR TEST

--- lib_test/decompress_test.ml
+++ lib_test/decompress_test.ml
@@ -119,7 +119,7 @@ module Deflate =
 struct
   include Deflate.Make(ExtString)(ExtBytes)
 
-  let string str size_i size_o =
+  let string str size_i size_o level =
     let i = Bytes.create size_i in
     let o = Bytes.create size_o in
     let compressed = Buffer.create (String.length str) in
@@ -138,71 +138,101 @@ struct
       size
     in
 
-    compress (Bytes.unsafe_to_string i) o refill' flush';
+    compress ~level (Bytes.unsafe_to_string i) o refill' flush';
     Buffer.contents compressed
 end
 
-let c2d si so data =
+let c2d si so level data =
   CamlZip.Deflate.string data
   |> fun o -> Inflate.string o si so
 
-let d2c si so data =
-  Deflate.string data si so
+let d2c si so level data =
+  Deflate.string data si so level
   |> CamlZip.Inflate.string
 
-let d2d si so data =
-  Deflate.string data si so
+let d2d si so level data =
+  Deflate.string data si so level
   |> fun o -> Inflate.string o si so
 
 let make_string_test ?(save = false) idx size =
   let data = generate size in
   let size_input = Random.(int 2048 + 2) in
   let size_output = Random.(int 2048 + 2) in
-  [
-    Printf.sprintf "--input-size %04d --output-size %04d"
-      size_input size_output,
+  let make_with_level level =
+  [ Printf.sprintf "--input-size %04d --output-size %04d --level %d"
+      size_input size_output level,
     `Slow,
     (fun () ->
       Alcotest.(check string) data
-        (d2d size_input size_output data) data);
-    Printf.sprintf "--input-size %04d --output-size %04d --use-camlzip=out"
-      size_input size_output,
+        (d2d size_input size_output level data) data)
+  ; Printf.sprintf "--input-size %04d --output-size %04d --level %d \
+                    (minimal input/output)" 2 2 level,
     `Slow,
     (fun () ->
       Alcotest.(check string) data
-        (d2c size_input size_output data) data);
-    Printf.sprintf "--input-size %04d --output-size %04d --use-camlzip=in"
-      size_input size_output,
+        (d2d 2 2 level data) data)
+  ; Printf.sprintf "--input-size %04d --output-size %04d --level %d \
+                    (traditional input/output)" 1024 1024 level,
     `Slow,
     (fun () ->
       Alcotest.(check string) data
-        (c2d size_input size_output data) data);
+        (d2d 1024 1024 level data) data)
+  ; Printf.sprintf "--input-size %04d --output-size %04d --level %d \
+                    --use-camlzip=out"
+      size_input size_output level,
+    `Slow,
+    (fun () ->
+      Alcotest.(check string) data
+        (d2c size_input size_output level data) data)
+  ; Printf.sprintf "--input-size %04d --output-size %04d --level %d \
+                    --use-camlzip=in"
+      size_input size_output level,
+    `Slow,
+    (fun () ->
+      Alcotest.(check string) data
+        (c2d size_input size_output level data) data);
   ]
+  in (List.map make_with_level [1; 2; 3; 4; 5; 6; 7; 8; 9] |> List.concat)
 
 let make_file_test filename =
   let data = load_file filename in
   let size_input = Random.(int 2048 + 2) in
   let size_output = Random.(int 2048 + 2) in
-  [
-    Printf.sprintf "--input-size %04d --output-size %04d %s"
-      size_input size_output filename,
+  let make_with_level level =
+  [ Printf.sprintf "--input-size %04d --output-size %04d --level %d %s"
+      size_input size_output level filename,
     `Slow,
     (fun () ->
       Alcotest.(check string) data
-        (d2d size_input size_output data) data);
-    Printf.sprintf "--input-size %04d --output-size %04d --use-camlzip=out %s"
-      size_input size_output filename,
+        (d2d size_input size_output level data) data)
+  ; Printf.sprintf "--input-size %04d --output-size %04d --level %d %s \
+                    (minimal input/output)" 2 2 level filename,
     `Slow,
     (fun () ->
       Alcotest.(check string) data
-        (d2c size_input size_output data) data);
-    Printf.sprintf "--input-size %04d --output-size %04d --use-camlzip=in  %s"
-      size_input size_output filename,
+        (d2d 2 2 level data) data)
+  ; Printf.sprintf "--input-size %04d --output-size %04d --level %d %s \
+                    (traditional input/output)" 1024 1024 level filename,
     `Slow,
     (fun () ->
       Alcotest.(check string) data
-        (c2d size_input size_output data) data);
+        (d2d 1024 1024 level data) data)
+  ; Printf.sprintf "--input-size %04d --output-size %04d --level %d \
+                    --use-camlzip=out %s"
+      size_input size_output level filename,
+    `Slow,
+    (fun () ->
+      Alcotest.(check string) data
+        (d2c size_input size_output level data) data)
+  ; Printf.sprintf "--input-size %04d --output-size %04d --level %d \
+                    --use-camlzip=in  %s"
+      size_input size_output level filename,
+    `Slow,
+    (fun () ->
+      Alcotest.(check string) data
+        (c2d size_input size_output level data) data);
   ]
+  in (List.map make_with_level [1; 2; 3; 4; 5; 6; 7; 8; 9] |> List.concat)
 
 let test_files directory =
   files directory
