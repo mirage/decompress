aa3bd1fbfb6792f8d91f76efff4295ea1c4027d0
Romain Calascibetta <romain.calascibetta@gmail.com>
2016-07-14 07:54:48 -0700

    Use memcpy implementation in ocaml instead C implementation (avoid complex compilation and distribution scheme)

--- lib/memcpy.mli
+++ lib/memcpy.mli
@@ -2,5 +2,5 @@ open Bigarray
 
 type bigstring = (char, int8_unsigned_elt, c_layout) Array1.t
 
-val memcpy_bytes : bytes -> bytes -> int -> int -> int -> unit
+val memcpy_bytes     : bytes -> bytes -> int -> int -> int -> unit
 val memcpy_bigstring : bigstring -> bigstring -> int -> int -> int -> unit
