645f1a1dfe87ac0e21a005d540528a54fe9732d3
Romain Calascibetta <romain.calascibetta@gmail.com>
2016-07-17 04:53:46 -0700

    Some option for flambda

--- lib/memcpy.ml
+++ lib/memcpy.ml
@@ -7,8 +7,11 @@ let memcpy_bytes
   = fun src dst len src_off dst_off ->
   for i = 0 to len - 1
   do Bytes.set dst (dst_off + i) (Bytes.get src (src_off + i)) done
+[@@specialize]
+
 let memcpy_bigstring
   : bigstring -> bigstring -> int -> int -> int -> unit
   = fun src dst len src_off dst_off ->
   for i = 0 to len - 1
   do Bigarray.Array1.set dst (dst_off + i) (Bigarray.Array1.get src (src_off + i)) done
+[@@specialize]
