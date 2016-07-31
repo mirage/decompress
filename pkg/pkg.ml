e3f070a04d35a03d9db6275a5cad0595c97f5980
Romain Calascibetta <romain.calascibetta@gmail.com>
2016-07-31 16:31:56 -0700

    Remove conditionnal install of dpipe (with unix system)

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -9,14 +9,11 @@ let builder c os files =
   OS.Cmd.run @@
   Cmd.(ocamlbuild % "-use-ocamlfind" % "-plugin-tag" % "package(str)" %% of_list files)
 
-let unix = Conf.with_pkg ~default:false "unix"
-
 let () =
   let build = Pkg.build ~cmd:builder () in
   Pkg.describe "decompress" ~build @@ fun c ->
-  let unix = Conf.value c unix in
 
   Ok [ Pkg.lib ~exts:Exts.library "lib/decompress"
      ; Pkg.lib ~exts:Exts.c_dll_library "lib/libdecompress"
-     ; Pkg.bin ~built:unix "bin/dpipe"
+     ; Pkg.bin "bin/dpipe"
      ; Pkg.test "lib_test/decompress_test" ]
