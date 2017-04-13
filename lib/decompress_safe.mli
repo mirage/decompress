886e7c77b4fb58db04cf4198d5e0e27df5561ec2
Romain Calascibetta <romain.calascibetta@gmail.com>
2017-04-13 00:05:55 -0700

    Update safe module by the last commit (to add blit2 function)

--- lib/decompress_safe.mli
+++ lib/decompress_safe.mli
@@ -13,9 +13,11 @@ val set       : ([> write ], 'i) t -> int -> char -> unit
 val get_u16   : ([> read ], 'i) t -> int -> int
 val get_u32   : ([> read ], 'i) t -> int -> int32
 val get_u64   : ([> read ], 'i) t -> int -> int64
-val sub       : ([> read ], 'i) t -> int -> int -> 'i Decompress_b.t
+val sub_ro    : ([> read ], 'i) t -> int -> int -> (read, 'i) t
+val sub_rw    : ([> read ], 'i) t -> int -> int -> ([ read | write ], 'i) t
 val fill      : ([> write ], 'i) t -> int -> int -> char -> unit
 val blit      : ([> read ], 'i) t -> int -> ([> write ], 'i) t -> int -> int -> unit
+val blit2      : ([> read ], 'i) t -> int -> ([> write ], 'i) t -> int -> ([> write ], 'i) t -> int -> int -> unit
 val pp        : Format.formatter -> ([> read ], 'i) t -> unit
 val to_string : ([> read ], 'i) t -> string
 val adler32   : ([> read ], 'i) t -> int32 -> int -> int -> int32
