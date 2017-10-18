db86cf8a57ab1b4fb21e10f99093bdae425d48db
Romain Calascibetta <romain.calascibetta@gmail.com>
2017-10-18 14:19:50 -0700

    Add option for afl to build the fuzzer

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -2,5 +2,7 @@ open Ocamlbuild_plugin
 
 let () = dispatch @@ function
   | After_hygiene ->
-    pdep [ "link" ] "linkdep" (fun param -> [ param ])
+    pdep [ "link" ] "linkdep" (fun param -> [ param ]);
+    flag ["use_afl"; "compile"; "native"]
+      (S [ A "-afl-instrument" ])
   | _ -> ()
