970c074d8e9cf15d1db7c7b4007105d5ce70ae61
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-03-22 16:15:44 +0100

    Avoid warning when we use Bigarray.Array1.map_file

--- test/test.ml
+++ test/test.ml
@@ -28,7 +28,7 @@ let string_of_file filename =
 
 let bigstring_of_file filename =
   let i = Unix.openfile filename [ Unix.O_RDONLY ] 0o644 in
-  let m = Bigarray.Array1.map_file i Bigarray.Char Bigarray.c_layout false (-1) in
+  let m = (Bigarray.Array1.map_file [@warning "-3"]) i Bigarray.Char Bigarray.c_layout false (-1) in
   at_exit (fun () -> Unix.close i);
   m
 
