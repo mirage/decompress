0eaa2b40aeab8e7e4abf0193c18bbfa7d421a85f
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-07-12 11:32:25 +0200

    Update README.md and easy.ml

--- README.md
+++ README.md
@@ -2,23 +2,27 @@ Decompress - Pure OCaml implementation of Zlib
 ==============================================
 
 [![Build Status](https://travis-ci.org/mirage/decompress.svg?branch=master)](https://travis-ci.org/mirage/decompress)
-![MirageOS](https://img.shields.io/badge/MirageOS-%F0%9F%90%AB-red.svg)
 
 Decompress is a pure implementation of `zlib`. The goal is to create an
-available package for Mirage OS for `zlib` in OCaml (instead a C code).
+available package for Mirage OS which implements `zlib` in OCaml (instead a C
+code).
 
 We respect the interface of `zlib` and all flush mode is available
 (experimental):
 
-- `SYNC_FLUSH`
-- `PARTIAL_FLUSH`
-- `FULL_FLUSH`
+- `sync` performs the following tasks:
+ - if there is some buffered but not yet compressed data, then this data is
+   compressed into one or several blocks
+ - a new type 0 block with empty contents is appended
+- `partial` is a deprecated flush method
+- `full` is a variant of the `sync` method flush. The difference lies in the
+  LZ77 step. The full flush is a sync flush where the dictionary is emptied:
+  after a full flush, the deflater will refrain from using copy symbols which
+  reference sequences appearing before the flush point.
 
 The interface proposed is a non-blocking interface.
 
 Home page: http://din.osau.re/
-
-Documentation: https://mirage.github.io/decompress/api.docdir/
 
 Contact: Romain Calascibetta `<romain.calascibet ta@gmail.com>`
 
@@ -27,15 +31,38 @@ Contact: Romain Calascibetta `<romain.calascibet ta@gmail.com>`
 Decompress can be installed with `opam`:
 
     opam install decompress
+    
+## Checkseum & Optint, linking with Decompress
+
+From benchmarks, the biggest bottleneck of `decompress` seems to be the
+computation of the ADLER-32. From this acknowledge, we decide to externalize
+this part of `decompress` to 2 sub-libraries:
+[`checkseum`](https://github.com/dinosaure/checkseum.git) and
+[`optint`](https://github.com/dinosaure/optint.git).
+
+`checkseum` (and, by this way, `decompress`) uses a trick about linking and let
+the end-user to choose which implementation he wants. We provide 2
+implementations: `checkseum.c` and `checkseum.ocaml`. Currently, `decompress`
+**does not** choose an implementation.
+
+When you want to use `decompress`, you **must** choose which implementation
+you want and link with `decompress` **and** `checkseum.{c,ocaml}`.
+
+## RFC 1951
+
+This distribution provides an implementation of `zlib` and an implementation of
+[RFC 1951](https://www.ietf.org/rfc/rfc1951.txt) - which is a subset of `zlib`.
+You can use both if you link with `decompress` - or just use the RFC 1951
+implementation by the `rfc1951` package.
+
+The biggest difference between `zlib` and `rfc1951` is:
+- no header
+- input/output is not aligned on byte
+- no checksum
 
 ## Sample programs
 
-Sample program are located in the `bin` directory of the distribution. It can be
-built with (dependancy with Unix module, the `cmdliner` package and a C code):
-
-    ocamlbuild -use-ocamlfind bin/dpipe.native
-
-Another good example is provided in `bin/easy.ml` with the signature:
+A good example is provided in `bin/easy.ml` with the signature:
 
 ```ocaml
 val compress   : ?level:int -> string -> string
@@ -44,24 +71,15 @@ val uncompress : string -> string
 
 And you can compile this program with:
 
-    ocamlbuild -use-ocamlfind bin/easy.native
+    ocamlbuild -use-ocamlfind -package checkseum.c,decompress bin/easy.native
 
 But keep in your mind, it's an easy example and it's not optimized for a
 productive environment - so, don't copy/paste and think.
-
-The documentation is
-available [online](https://mirage.github.io/decompress/api.docdir/) to
-understand how to use Decompress.
 
 ## Build Requirements
 
  * OCaml >= 4.03.0
  * `base-bytes` meta-package
  * Bigarray module (provided by the standard library of OCaml)
- * `topkg`, `ocamlfind` and `ocamlbuild` to build the project
-
-If you want to compile the test program, you need:
-
- * `camlzip` to compare `decompress` with `zlib`
- * `re.1.7.1`
- * `alcotest`
+ * `dune` to build the project
+ * `checkseum` & `optint` to compute ADLER-32 checksum
--- bin/easy.ml
+++ bin/easy.ml
@@ -4,33 +4,29 @@ exception Deflate_error of Zlib_deflate.error
 exception Inflate_error of Zlib_inflate.error
 
 (* Keep in your mind, this is an easy example of Decompress but not efficient.
-   Don't copy/paste this code in a productive environment.
-*)
+   Don't copy/paste this code in a productive environment. *)
 
 let compress ?(level = 4) data =
   let input_buffer = Bytes.create 0xFFFF in
   (* We need to allocate an input buffer, the size of this buffer is important.
-     In fact, the Lz77 algorithm can find a pattern (and compress) only on
-     this input buffer. So if the input buffer is small, the algorithm has no
-     chance to find many patterns.
+     In fact, the Lz77 algorithm can find a pattern (and compress) only on this
+     input buffer. So if the input buffer is small, the algorithm has no chance
+     to find many patterns.
 
-     If it is big, the algorithm can find a far pattern and keep this pattern
-     as long as it tries to compress. The optimal size seems to be [1 << 15]
-     bytes (a bigger buffer is not necessary because the distance can be upper
-     than [1 << 15]).
-  *)
+     If it is big, the algorithm can find a far pattern and keep this pattern as
+     long as it tries to compress. The optimal size seems to be [1 << 15] bytes
+     (a bigger buffer is not necessary because the distance can be upper than [1
+     << 15]). *)
   let output_buffer = Bytes.create 0xFFFF in
   (* We need to allocate an output buffer, is like you can. it's depends your
-     capabilities of your writing.
-  *)
+     capabilities of your writing. *)
 
   let pos = ref 0 in
   let res = Buffer.create (String.length data) in
   (* The buffer is not a good idea. In fact, we can have a memory problem with
      that (like if the output is too big). You need to keep in your mind that is
      insecure to let a buffer to grow automatically (an attacker can use this
-     behaviour).
-  *)
+     behaviour). *)
 
   (* This is the same interface as [caml-zip]. A refiller and a flusher. The
      refiller send you the maximum byte than you can [blit] inside the input
@@ -42,22 +38,21 @@ let compress ?(level = 4) data =
 
      The flusher send you the output buffer and how many byte Decompress wrote
      inside. The offset for this buffer is always [0]. Then, you need to send
-     how many bytes are free in the output buffer (and the common is that all
-     is free).
+     how many bytes are free in the output buffer (and the common is that all is
+     free).
 
-     One argument (optionnal) is missing, it's the [meth]. This argument is
-     used to limit the memory used by the state internally. In fact, Decompress
-     (and `zlib`) need to keep all of your input to calculate at the end the
-     frequencies and the dictionarie. So if you want to compress a big file,
-     may be you will have a memory problem (because, all your file will be
-     present in the memory). So you can specify a method to flush the internal
-     memory (with SYNC, PARTIAL or FULL - see the documentation about that) at
-     each [n] bytes, like: ~meth:(PARTIAL, 4096) flushes the internal memory
-     when we compute 4096 bytes of your input.
+     One argument (optionnal) is missing, it's the [meth]. This argument is used
+     to limit the memory used by the state internally. In fact, Decompress (and
+     `zlib`) need to keep all of your input to calculate at the end the
+     frequencies and the dictionary. So if you want to compress a big file, may
+     be you will have a memory problem (because, all your file will be present
+     in the memory). So you can specify a method to flush the internal memory
+     (with SYNC, PARTIAL or FULL - see the documentation about that) at each [n]
+     bytes, like: ~meth:(PARTIAL, 4096) flushes the internal memory when we
+     compute 4096 bytes of your input.
 
      If [meth] is specified, the refiller has a [Some] as the second parameter.
-     Otherwise, it is [None].
-  *)
+     Otherwise, it is [None]. *)
   Zlib_deflate.bytes
     input_buffer output_buffer
     (fun input_buffer -> function
@@ -76,17 +71,15 @@ let compress ?(level = 4) data =
       0xFFFF)
     (Zlib_deflate.default ~proof:B.proof_bytes level)
   (* We can specify the level of the compression, see the documentation to know
-     what we use for each level. The default is 4.
-  *)
+     what we use for each level. The default is 4. *)
   |> function
      | Ok _ -> Buffer.contents res
      | Error exn -> raise (Deflate_error exn)
 
 let uncompress data =
   let input_buffer = Bytes.create 0xFFFF in
-  (* We need to allocate an input buffer. it's depends your capabilities of
-     your reading.
-  *)
+  (* We need to allocate an input buffer. it's depends your capabilities of your
+     reading. *)
   let output_buffer = Bytes.create 0xFFFF in
   (* Same as [compress]. *)
   let window = Window.create ~proof:B.proof_bytes in
@@ -95,8 +88,7 @@ let uncompress data =
      allocate this buffer costs.
 
      So in this case, we decompress only one time but if you want to decompress
-     some flows, you can reuse this window after a [Window.reset].
-  *)
+     some flows, you can reuse this window after a [Window.reset]. *)
 
   let pos = ref 0 in
   let res = Buffer.create (String.length data) in
