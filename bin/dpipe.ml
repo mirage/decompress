a876b45fa1d3ba9aa8950bb13e1e69838e34103c
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-09-05 13:54:37 +0200

    ocamlformat pass

--- bin/dpipe.ml
+++ bin/dpipe.ml
@@ -2,73 +2,90 @@ let () = Printexc.record_backtrace true
 
 module B = Decompress.B
 
-external bs_read : Unix.file_descr -> B.Bigstring.t -> int -> int -> int =
-  "bigstring_read" [@@noalloc]
-external bs_write : Unix.file_descr -> B.Bigstring.t -> int -> int -> int =
-  "bigstring_write" [@@noalloc]
+external bs_read :
+  Unix.file_descr -> B.Bigstring.t -> int -> int -> int
+  = "bigstring_read"
+  [@@noalloc]
+
+external bs_write :
+  Unix.file_descr -> B.Bigstring.t -> int -> int -> int
+  = "bigstring_write"
+  [@@noalloc]
 
 (** Abstract [Unix.read] with ['a B.t]. *)
-let unix_read
-  : type a. a B.t -> Unix.file_descr -> a -> int -> int -> int = function
+let unix_read : type a. a B.t -> Unix.file_descr -> a -> int -> int -> int =
+  function
   | B.Bytes -> Unix.read
   | B.Bigstring -> bs_read
 
-let unix_write
-  : type a. a B.t -> Unix.file_descr -> a -> int -> int -> int = function
+let unix_write : type a. a B.t -> Unix.file_descr -> a -> int -> int -> int =
+  function
   | B.Bytes -> Unix.write
   | B.Bigstring -> bs_write
 
 let _chunk = 0xFFFF
 
 let do_command input_size output_size mode level wbits =
-  let src = Bigarray.Array1.create Bigarray.Char Bigarray.c_layout input_size in
-  let dst = Bigarray.Array1.create Bigarray.Char Bigarray.c_layout output_size in
-
+  let src =
+    Bigarray.Array1.create Bigarray.Char Bigarray.c_layout input_size
+  in
+  let dst =
+    Bigarray.Array1.create Bigarray.Char Bigarray.c_layout output_size
+  in
   match mode with
-  | `Compression ->
-    let t = Decompress.Zlib_deflate.default ~witness:B.bigstring ~wbits level in
-    let r = Decompress.Zlib_deflate.to_result
-      src dst
-      (fun src -> function
-       | Some max -> unix_read B.bigstring Unix.stdin src 0 (min max input_size)
-       | None -> unix_read B.bigstring Unix.stdin src 0 input_size)
-      (fun dst len -> let _ = unix_write B.bigstring Unix.stdout dst 0 len in output_size)
-      t
-    in (match r with
-        | Ok _ -> ()
-        | Error exn -> Format.eprintf "%a\n%!" Decompress.Zlib_deflate.pp_error exn)
-  | `Decompression ->
-    let w = Decompress.Window.create ~witness:B.bigstring in
-    let t = Decompress.Zlib_inflate.default ~witness:B.bigstring w in
-    let r = Decompress.Zlib_inflate.to_result
-      src dst
-      (fun src -> unix_read B.bigstring Unix.stdin src 0 input_size)
-      (fun dst len ->
-       let _ = unix_write B.bigstring Unix.stdout dst 0 len in output_size)
-      t
-    in (match r with
-        | Ok _ -> ()
-        | Error exn -> Format.eprintf "%a\n%!" Decompress.Zlib_inflate.pp_error exn)
+  | `Compression -> (
+      let t =
+        Decompress.Zlib_deflate.default ~witness:B.bigstring ~wbits level
+      in
+      let r =
+        Decompress.Zlib_deflate.to_result src dst
+          (fun src -> function
+            | Some max ->
+                unix_read B.bigstring Unix.stdin src 0 (min max input_size)
+            | None -> unix_read B.bigstring Unix.stdin src 0 input_size )
+          (fun dst len ->
+            let _ = unix_write B.bigstring Unix.stdout dst 0 len in
+            output_size )
+          t
+      in
+      match r with
+      | Ok _ -> ()
+      | Error exn ->
+          Format.eprintf "%a\n%!" Decompress.Zlib_deflate.pp_error exn )
+  | `Decompression -> (
+      let w = Decompress.Window.create ~witness:B.bigstring in
+      let t = Decompress.Zlib_inflate.default ~witness:B.bigstring w in
+      let r =
+        Decompress.Zlib_inflate.to_result src dst
+          (fun src -> unix_read B.bigstring Unix.stdin src 0 input_size)
+          (fun dst len ->
+            let _ = unix_write B.bigstring Unix.stdout dst 0 len in
+            output_size )
+          t
+      in
+      match r with
+      | Ok _ -> ()
+      | Error exn ->
+          Format.eprintf "%a\n%!" Decompress.Zlib_inflate.pp_error exn )
 
 open Cmdliner
 
 let nat a b =
   let parse s =
-    try let v = int_of_string s in
-        let p = Printf.sprintf in
-        match v, a, b with
-        | v, Some a, Some b when a <= v && v <= b -> `Ok v
-        | v, Some a, None when a <= v -> `Ok v
-        | v, None, Some b when v <= b -> `Ok v
-        | v, None, None -> `Ok v
-        | _, Some a, Some b ->
-          failwith (p "%s must be >= %d and <= %d" s a b)
-        | _, Some a, None ->
-          failwith (p "%s must be >= %d" s a)
-        | _, None, Some b ->
-          failwith (p "%s must be <= %d" s b)
+    try
+      let v = int_of_string s in
+      let p = Printf.sprintf in
+      match v, a, b with
+      | v, Some a, Some b when a <= v && v <= b -> `Ok v
+      | v, Some a, None when a <= v -> `Ok v
+      | v, None, Some b when v <= b -> `Ok v
+      | v, None, None -> `Ok v
+      | _, Some a, Some b -> failwith (p "%s must be >= %d and <= %d" s a b)
+      | _, Some a, None -> failwith (p "%s must be >= %d" s a)
+      | _, None, Some b -> failwith (p "%s must be <= %d" s b)
     with Failure e -> `Error e
-  in parse, Format.pp_print_int
+  in
+  parse, Format.pp_print_int
 
 let mode =
   let parse = function
@@ -76,9 +93,10 @@ let mode =
     | "inflate" -> `Ok `Decompression
     | _ -> `Error "Invalid mode"
   in
-  parse, (fun fmt -> function
-          | `Compression -> Format.pp_print_string fmt "compression"
-          | `Decompression -> Format.pp_print_string fmt "decompression")
+  ( parse
+  , fun fmt -> function
+      | `Compression -> Format.pp_print_string fmt "compression"
+      | `Decompression -> Format.pp_print_string fmt "decompression" )
 
 let mode =
   let doc = "Compression or decompression" in
@@ -103,13 +121,12 @@ let output_size =
 let command =
   let doc = "Deflate and inflate any document." in
   let man =
-  [ `S "Description"
-  ; `P "$(tname) takes a standard input and write in standard output the \
-        compressed/uncompressed data." ]
+    [ `S "Description"
+    ; `P
+        "$(tname) takes a standard input and write in standard output the \
+         compressed/uncompressed data." ]
   in
-  Term.(pure do_command $ input_size $ output_size $ mode $ level $ wbits),
-  Term.info "dpipe" ~doc ~man
+  ( Term.(pure do_command $ input_size $ output_size $ mode $ level $ wbits)
+  , Term.info "dpipe" ~doc ~man )
 
-let () = match Term.eval command with
-  | `Error _ -> exit 1
-  | _ -> exit 0
+let () = match Term.eval command with `Error _ -> exit 1 | _ -> exit 0
--- lib/decompress_safe.mli
+++ lib/decompress_safe.mli
@@ -1,26 +1,45 @@
 module B = Decompress_b
 
-type ro = [ `Rd ]
-type wo = [ `Wr ]
+type ro = [`Rd]
+type wo = [`Wr]
+type ('a, 'i) t = private 'i constraint 'a = [< `Rd | `Wr]
 
-type ('a, 'i) t = private 'i constraint 'a = [< `Rd | `Wr ]
-
-val rw : 'i B.t -> 'i -> ([ ro | wo ], 'i) t
+val rw : 'i B.t -> 'i -> ([ro | wo], 'i) t
 val ro : 'i B.t -> 'i -> (ro, 'i) t
 val wo : 'i B.t -> 'i -> (wo, 'i) t
-
 val length : 'i B.t -> ('a, 'i) t -> int
-val get : 'i B.t -> ([> ro ], 'i) t -> int -> char
-val set : 'i B.t -> ([> wo ], 'i) t -> int -> char -> unit
-val get_16 : 'i B.t -> ([> ro ], 'i) t -> int -> int
-val get_32 : 'i B.t -> ([> ro ], 'i) t -> int -> int32
-val get_64 : 'i B.t -> ([> ro ], 'i) t -> int -> int64
-val sub_ro : 'i B.t -> ([> ro ], 'i) t -> int -> int -> (ro, 'i) t
-val sub_rw : 'i B.t -> ([> ro ], 'i) t -> int -> int -> ([ ro | wo ], 'i) t
-val fill : 'i B.t -> ([> wo ], 'i) t -> int -> int -> char -> unit
-val blit : 'i B.t -> ([> ro ], 'i) t -> int -> ([> wo ], 'i) t -> int -> int -> unit
-val blit2 : 'i B.t -> ([> ro ], 'i) t -> int -> ([> wo ], 'i) t -> int -> ([> wo ], 'i) t -> int -> int -> unit
-val pp : 'i B.t -> Format.formatter -> ([> ro ], 'i) t -> unit
-val to_string : 'i B.t -> ([> ro ], 'i) t -> string
-val adler32 : 'i B.t -> ([> ro ], 'i) t -> int -> int -> Checkseum.Adler32.t -> Checkseum.Adler32.t
+val get : 'i B.t -> ([> ro], 'i) t -> int -> char
+val set : 'i B.t -> ([> wo], 'i) t -> int -> char -> unit
+val get_16 : 'i B.t -> ([> ro], 'i) t -> int -> int
+val get_32 : 'i B.t -> ([> ro], 'i) t -> int -> int32
+val get_64 : 'i B.t -> ([> ro], 'i) t -> int -> int64
+val sub_ro : 'i B.t -> ([> ro], 'i) t -> int -> int -> (ro, 'i) t
+val sub_rw : 'i B.t -> ([> ro], 'i) t -> int -> int -> ([ro | wo], 'i) t
+val fill : 'i B.t -> ([> wo], 'i) t -> int -> int -> char -> unit
+
+val blit :
+  'i B.t -> ([> ro], 'i) t -> int -> ([> wo], 'i) t -> int -> int -> unit
+
+val blit2 :
+     'i B.t
+  -> ([> ro], 'i) t
+  -> int
+  -> ([> wo], 'i) t
+  -> int
+  -> ([> wo], 'i) t
+  -> int
+  -> int
+  -> unit
+
+val pp : 'i B.t -> Format.formatter -> ([> ro], 'i) t -> unit
+val to_string : 'i B.t -> ([> ro], 'i) t -> string
+
+val adler32 :
+     'i B.t
+  -> ([> ro], 'i) t
+  -> int
+  -> int
+  -> Checkseum.Adler32.t
+  -> Checkseum.Adler32.t
+
 val unsafe : ('a, 'i) t -> 'i
