3d15ca974318adabf17aecd3d2b0ecd777520931
Patrick Ferris <patrick@sirref.org>
2023-09-16 14:45:25 +0100

    Allow different levels

--- lib/lzw.ml
+++ lib/lzw.ml
@@ -5,6 +5,8 @@ module Io = struct
   type dst = (bytes * int * int) option -> unit
 end
 
+let eof = 256
+
 module Dictionary : sig
   type t
 
@@ -60,18 +62,17 @@ let ux_soi = max_int - 1 (* Start of input, outside unicode range. *)
 type src = {
   src : Io.src;
   d : Dictionary.t;
-  level : int;
+  level : int; (* Max bit size *)
   mutable i : bytes; (* Current input chunk. *)
   mutable i_pos : int; (* Next input position to read. *)
   mutable i_max : int; (* Maximal input position to read. *)
   mutable c : int;
+  mutable extra_bits : int * int; (* The bits left-aligned and how many *)
   mutable rbuf : Buffer.t;
   mutable buf : Buffer.t;
 }
 
-let badd d = Buffer.add_char d.rbuf (Char.chr d.c)
-
-let src ?(level=16) src =
+let src ?(level = 12) src =
   {
     src;
     level;
@@ -82,6 +83,7 @@ let src ?(level=16) src =
     buf = Buffer.create 256;
     rbuf = Buffer.create 8;
     c = ux_soi;
+    extra_bits = (0, 0);
   }
 
 let refill d =
@@ -109,12 +111,23 @@ type dst = {
   mutable o : bytes; (* Current output chunk. *)
   mutable o_pos : int; (* Next output position to write. *)
   mutable o_max : int; (* Maximal output position to write. *)
+  mutable pending : int * int;
 }
 
-let dst ?(level=16) ?(buf = Bytes.create io_buffer_size) dst =
+let dst ?(level = 12) ?(buf = Bytes.create io_buffer_size) dst =
   let o_max = Bytes.length buf - 1 in
   if o_max = 0 then invalid_arg "buf's length is empty"
-  else { dst; o = buf; buff = Buffer.create 128; o_pos = 0; o_max; level; scratch = Bytes.create 2 }
+  else
+    {
+      dst;
+      o = buf;
+      buff = Buffer.create 128;
+      o_pos = 0;
+      o_max;
+      level;
+      scratch = Bytes.create 2;
+      pending = (0, 0);
+    }
 
 let flush e ~stop =
   if stop then (
@@ -131,10 +144,19 @@ let rec writec e c =
     Bytes.set_uint8 e.o e.o_pos c;
     e.o_pos <- e.o_pos + 1)
 
-let w_uint16_be dst c =
-  Bytes.set_uint16_ne dst.scratch 0 c;
-  writec dst (Bytes.get_uint8 dst.scratch 0);
-  writec dst (Bytes.get_uint8 dst.scratch 1)
+let w_flush dst v =
+  while snd dst.pending >= v do
+    let output = fst dst.pending in
+    writec dst (output land 0xff);
+    let i = output lsr 8 in
+    dst.pending <- (i, snd dst.pending - 8)
+  done
+
+let w_level dst c =
+  let out, bits = dst.pending in
+  let output = out lor (c lsl bits) in
+  dst.pending <- (output, bits + dst.level);
+  w_flush dst 8
 
 let compress src dst =
   try
@@ -151,7 +173,7 @@ let compress src dst =
           match Dictionary.lookup src.d s_old with
           | None -> assert false
           | Some c ->
-              w_uint16_be dst c;
+              w_level dst c;
               Buffer.reset src.buf;
               Buffer.add_uint8 src.buf chr)
     done
@@ -162,24 +184,32 @@ let compress src dst =
       match Dictionary.lookup src.d s with
       | None -> assert false
       | Some c ->
-          w_uint16_be dst c;
+          w_level dst c;
+          writec dst eof;
           flush dst ~stop:true)
 
-let r_uint16_be s =
-  readc s;
-  badd s;
-  readc s;
-  badd s;
-  Buffer.to_bytes s.rbuf |> fun b ->
-  Buffer.clear s.rbuf;
-  Bytes.get_uint16_ne b 0
+let r_level s =
+  (* Make sure we have enough bytes to grab the data *)
+  while snd s.extra_bits < s.level do
+    readc s;
+    let pending_input = fst s.extra_bits in
+    let new_input = pending_input lor ((s.c land 0xFF) lsl snd s.extra_bits) in
+    s.extra_bits <- (new_input, snd s.extra_bits + 8)
+  done;
+  let pending_input = fst s.extra_bits in
+  let available = snd s.extra_bits in
+  let i = pending_input land lnot (lnot 0 lsl s.level) in
+  let r = pending_input lsr s.level in
+  let m = available - s.level in
+  s.extra_bits <- (r, m);
+  if i = eof then raise End_of_file else i
 
 let decompress src dst =
   let d = Reverse_dictionary.v () in
   let previous_string = ref Bytes.empty in
   try
     while true do
-      let code = r_uint16_be src in
+      let code = r_level src in
       if Option.is_none (Reverse_dictionary.lookup d code) then
         Reverse_dictionary.raw_add d code
           (Bytes.cat !previous_string
